cecb4743af2bde15cb72281f986a5fc9efaf6173
Alfonso <contrerasalfonso.lino@gmail.com>
2024-04-16 10:04:23 -0500

    first commit

---
+++ src/pages/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <q-form @submit="enviarForm" class="q-pa-xl">
+      <q-input v-model="usuario" placeholder="ingresa tu usuario" type="text" />
+      <q-input v-model="password" placeholder="ingresa tu password" type="text" />
+      <p class="text-pink-9">Test ...</p>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      usuario: '',
+      password: ''
+    }
+  },
+  methods:{
+    enviarForm(){
+
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/Test.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <q-form @submit="enviarData">
+      <q-input filled v-model="date" mask="date" :rules="['date']">
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy
+              cover
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date v-model="date">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Close" color="primary" flat />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+      <q-btn color="secondary" type="submit">Envio de info</q-btn>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: "2030/02/01",
+    };
+  },
+
+  methods:{
+    enviarData(){
+      const numero = 2;
+      console.log(this.date)
+      console.log(numero)
+      console.log('enviando data....')
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.rojo {
+}
+</style>
